1c2ae0d2585ed3906a92a1050eff2fb48c6ac2d8
Lerato03 <105088269+Lerato03@users.noreply.github.com>
2022-06-04 03:53:54 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,9 +14,9 @@ int main(void)
 	if (n % 10 > 5)
 		printf("and is greater than 5\n");
 	else if (n % 10 == 0)
-	printf(and is 0\n");
+	printf(n is 0\n");
 	else
 		printf("and is less than 6 and not 0\n");
 	
-	return (0);
+	return(0);
 }
