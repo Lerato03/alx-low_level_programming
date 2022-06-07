f0776848f1d5dc237a55917130b559711eee0581
Lerato03 <Leratoinnocensia876@gmail.com>
2022-06-06 17:47:12 -0700

    printf c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-
-/**
- * main -prints the size of various types
- * Return: 0 if exited proparly, non-zero otherwis
- */
-int main(void)
-{
-	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("size of an int: %d byte(s)\n", sizeof(int));
-	printf("size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
-}
+[master 7ec7100] size c
+ 2 files changed, 18 insertions(+)
+ create mode 100644 0x00-hello_world/6-size.c
+ create mode 100644 0x00-hello_world/README.md
