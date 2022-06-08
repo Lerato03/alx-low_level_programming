3525bb74f23dd09af1004cd2636ec6b5d4cb50c5
Lerato03 <Leratoinnocensia876@gmail.com>
2022-06-08 16:11:55 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,3 @@
-#include "main.h"
-
-/**
- * print_alphabet -prints the alphabet, in lowercase, followed by a new line
- */
-void print_alphabet(void)
-{
-	char ch;
-	ch = 'a';
-
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-	
-	_puchar('\n');
-}
+[master 00fdad1] 1-alphabet.c
+ 2 files changed, 21 insertions(+), 13 deletions(-)
+ create mode 100644 0x02-functions_nested_loops/1-alphabet.c
