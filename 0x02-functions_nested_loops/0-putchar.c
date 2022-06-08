cf1cbc5818abdda154dadda968365dee9e67ddad
Lerato03 <Leratoinnocensia876@gmail.com>
2022-06-08 01:21:35 -0700

    putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,3 @@
-On branch master
-Your branch and 'origin/master' have diverged,
-and have 1 and 2 different commits each, respectively.
-  (use "git pull" to merge the remote branch into yours)
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   0-putchar.c
-	modified:   ../README.md
-
-no changes added to commit (use "git add" and/or "git commit -a")
+[master 69435ec] 0-putchar.c
+ 2 files changed, 14 insertions(+), 21 deletions(-)
+ rewrite 0x02-functions_nested_loops/0-putchar.c (99%)
