e766286623b166b47f0ab407b1eef7f498e60f0d
Lerato03 <105088269+Lerato03@users.noreply.github.com>
2022-06-09 01:18:34 +0200

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-[master 00fdad1] 1-alphabet.c
- 2 files changed, 21 insertions(+), 13 deletions(-)
- create mode 100644 0x02-functions_nested_loops/1-alphabet.c
-=======
 #include "main.h"
 
 /**
@@ -22,4 +17,3 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
->>>>>>> 86f2309017da9eaba20d0b7711e77788338605f2
