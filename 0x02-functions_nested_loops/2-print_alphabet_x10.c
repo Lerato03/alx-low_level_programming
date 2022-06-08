d1c98e3f7faafa64557ec7ab2a3eded5e0fb1e04
Lerato03 <105088269+Lerato03@users.noreply.github.com>
2022-06-09 01:52:29 +0200

    2-prints_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,25 @@
-[master 3525bb7] 2-print_alphabet_x10.c
- 2 files changed, 28 insertions(+), 18 deletions(-)
- create mode 100644 0x02-functions_nested_loops/2-print_alphabet_x10.c
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+*followed by a new line
+*/
+void print_alphabet_x10(void)
+{
+ char ch;
+ int i;
+ 
+ i = 0
+  
+  while (i < 10)
+  {
+   ch = 'a';
+   while (ch <= 'z');
+   {
+    _putchar(ch);
+    ch++;
+   }
+   _putchar('\n');
+   i++;
+  }
+}
