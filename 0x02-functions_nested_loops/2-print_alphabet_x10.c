6bbd9b36bd97392302e99950724c562578be1777
Lerato03 <Leratoinnocensia876@gmail.com>
2022-06-08 17:13:30 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,3 +1,2 @@
-[master 3525bb7] 2-print_alphabet_x10.c
- 2 files changed, 28 insertions(+), 18 deletions(-)
- create mode 100644 0x02-functions_nested_loops/2-print_alphabet_x10.c
+[master 89b90e0] 2-print_alphabet_x10.c
+ 1 file changed, 3 insertions(+), 25 deletions(-)
