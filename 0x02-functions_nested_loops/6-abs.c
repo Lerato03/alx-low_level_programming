5fe18e475771beb15c9395252c46f8e20c371439
Lerato03 <105088269+Lerato03@users.noreply.github.com>
2022-06-09 05:15:44 +0200

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,3 +1,15 @@
-[master 0638f53] 6-abs.c
- 2 files changed, 18 insertions(+), 25 deletions(-)
- create mode 100644 0x02-functions_nested_loops/6-abs.c
+#include "main.h"
+
+/**
+*_abs - computes the absolute value of an integer
+*@n: the int to check
+*Return: the absolut value of in
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+ return (n);
+}
+return (-n);
+}
