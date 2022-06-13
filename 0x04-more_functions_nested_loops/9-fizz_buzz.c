f7f3116da57007f36105d337f72675124db6ddb0
Lerato03 <105088269+Lerato03@users.noreply.github.com>
2022-06-13 19:38:10 +0200

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,14 +16,14 @@ int main(void)
 	char fb[] = "fizzbuzz";
 
 
-	for(i = 1; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
 			printf("%s", b);
 		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s ", fb);
 		else if (i % 3 == 0)
-			printf("%s ",f);
+			printf("%s ", f);
 		else if (i % 5 == 0)
 			printf("%s ", b);
 		else
